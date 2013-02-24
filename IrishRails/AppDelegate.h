04f09a2ab0aca8cc8d8c01fe86a1e4c2cbaac37f
jaanussiim <jaanus@jaanussiim.com>
2013-02-24 18:09:49 +0200

    Initial project files

---
+++ IrishRails/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IrishRails
+//
+//  Created by Jaanus Siim on 2/24/13.
+//  Copyright (c) 2013 JaanusSiim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IrishRails/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IrishRails
+//
+//  Created by Jaanus Siim on 2/24/13.
+//  Copyright (c) 2013 JaanusSiim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
